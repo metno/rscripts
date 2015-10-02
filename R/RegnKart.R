a01720c06218c960de43c032ec89c9abfdd5ed03
Hans Olav Hygen <hohygen@gmail.com>
2015-10-02 14:25:49 +0200

    Update RegnKart.R
    La inn muligheten for returperioder

--- R/RegnKart.R
+++ R/RegnKart.R
@@ -28,20 +28,20 @@ require(RgoogleMaps)
 #VindKart(FD="07.02.2015",TD="08.02.2015",center=c(69,25),elementer="FFX")
 
 VindKart <- function(FD="15.11.2013",TD="17.11.2013",elementer="FFX",Stasjoner=NA,center= c(61, 8),zoom=6,MinGrense=NA){ 
-  #FFX gir høyeste middelvind, mens FGX gir høyeste vindkast
+  #FFX gir høyeste middelvind, mens FGX gir høyeste vindkast, RR gir nedbørmengden, RR_R gir returperioden for nedbørmengden
   #center= c(64.5, 10) #Trøndelag
   #center= c(61, 8) #Sørnorge
   #Stasjoner kan settes eksplisitt om en ønsker et spesielt utvalg, ellers hentes det dynamisk inn for alle stasjoner som har observert på tidspunktet
   #zoom=6 #Kan justeres for å sette ulike utsnitt, men er ikk trinnløs
-
+  
   Aar <- substr(FD,7,10)
   if (is.na(Stasjoner)){Stasjoner<- Stasjon.Laster(elementer=elementer,FY=Aar,TY=Aar)
-    Sperrede <- c(89920, 86070) #put station nuber of stations that shall be excluded here
-    LS <- length(Sperrede)
-    for (n in LS){
-      Stasjoner <- Stasjoner[Stasjoner[,6]!=Sperrede[n],]
-    }
-    Stasjoner <- Stasjoner[Stasjoner[,6]<=99999,]
+                        Sperrede <- c(89920, 86070) #put station nuber of stations that shall be excluded here
+                        LS <- length(Sperrede)
+                        for (n in LS){
+                          Stasjoner <- Stasjoner[Stasjoner[,6]!=Sperrede[n],]
+                        }
+                        Stasjoner <- Stasjoner[Stasjoner[,6]<=99989,]
   }
   St <- Stasjoner[,c(6,10,11)]
   ls <- length(St[,1])
@@ -54,18 +54,19 @@ VindKart <- function(FD="15.11.2013",TD="17.11.2013",elementer="FFX",Stasjoner=N
     if(elementer=="FFX"){try(Data <- Dogn.Laster(StNr=St[n,1],FD=FD,TD=TD,elementer=c("FFX","FXX")),silent=TRUE)}
     if(elementer=="FGX"){try(Data <- Dogn.Laster(StNr=St[n,1],FD=FD,TD=TD,elementer=c("FGX")),silent=TRUE)}
     if(elementer=="RR"){try(Data <- Dogn.Laster(StNr=St[n,1],FD=FD,TD=TD,elementer=c("RR")),silent=TRUE)}
+    if(elementer=="RR_R"){try(Data <- Dogn.Laster(StNr=St[n,1],FD=FD,TD=TD,elementer=c("RR")),silent=TRUE)}
     #print(St[n,1])
     #print(length(Data))
     if(length(Data>0)){
       if(elementer=="FFX"){
         VindMaks <- max(c(max(c(as.numeric(Data[,5]),na.rm=TRUE)),max(c(as.numeric(Data[,6]),na.rm=TRUE))),na.rm=TRUE)
-      #Farver settes her for middelvind, grenser i m/s
+        #Farver settes her for middelvind, grenser i m/s
         Farge<-"gray19"
         if (VindMaks > 32.6){Farge="darkred"}
         if (VindMaks > 28.4 & VindMaks<32.7){Farge="Purple"}
         if (VindMaks > 24.4 & VindMaks<28.5){Farge="darkblue"}
         if (VindMaks > 20.7 & VindMaks<24.5){Farge="darkgreen"}
-      print(c(St[n,1],Farge,VindMaks))
+        print(c(St[n,1],Farge,VindMaks))
       }
       if(elementer=="FGX"){
         VindMaks <- max(c(max(as.numeric(Data[,5]),na.rm=TRUE)),na.rm=TRUE)
@@ -97,7 +98,37 @@ VindKart <- function(FD="15.11.2013",TD="17.11.2013",elementer="FFX",Stasjoner=N
         if (VindMaks >= (50+Startpunkt) & VindMaks<(75+Startpunkt)){Farge="darkblue"}
         if (VindMaks >= (25+Startpunkt) & VindMaks<(50+Startpunkt)){Farge="darkgreen"}
       }
-#      TextOnStaticMap(Kart,lon=as.numeric(St$LON_DEC[n]),lat=as.numeric(St$LAT_DEC[n]),col='red',labels=VindMaks,adj=c(+1.5),cex=0.5,add=TRUE)
+      if(elementer=="RR_R"){
+        Data[is.na(Data[,5]),5]<-0
+        Data[Data[,5]<0,5]<-0
+        Nedbor.Mengde <- NA
+        NedborMengde <- sum(c(sum(as.numeric(Data[,5]),na.rm=TRUE)),na.rm=TRUE)
+        VindMaks <- NA
+        if(is.finite(NedborMengde)){
+          print(St[n,1])
+          NyUrl <- "http://klapp/kdvhpub/production/Report?re=51&head=&ct=text/plain&del=space&ddel=comma&no=0&nod=blank&nob=dot&flag=7&sflag=-1&qa=4&la=no&co=NO&m1=0&m1=1&m1=11&m2=2&m2=3&m2=4&m3=5&m3=6&m3=7&m4=8&m4=9&m4=10&num=10&nd=1&fac=1&nmt=1&lastperiod=true&pid=9&val="
+          NyUrl <- paste(NyUrl,NedborMengde,"&s=",St[n,1],sep="")
+          VindMaks <- NA
+          try(VindMaks<-scan(file=NyUrl,skip=12,n=2)[2],silent=TRUE)
+        }
+        print(VindMaks) 
+        
+        #Farver  settes her for vindkast, grenser i m/s
+        Farge<-"gray19"
+        #      if (VindMaks > 32.6*1.3){Farge="darkred"}
+        #      if (VindMaks > 28.4*1.3 & VindMaks<32.7*1.3){Farge="Purple"}
+        #      if (VindMaks > 24.4*1.3 & VindMaks<28.5*1.3){Farge="darkblue"}
+        #      if (VindMaks > 20.7*1.3 & VindMaks<24.5*1.3){Farge="darkgreen"}
+        if(is.finite(VindMaks)){
+          Startpunkt <- 0
+          if(!is.na(MinGrense)){Startpunkt<-MinGrense}
+          if (VindMaks > (100+Startpunkt)){Farge="darkred"}
+          if (VindMaks >= (75+Startpunkt) & VindMaks<(100+Startpunkt)){Farge="Purple"}
+          if (VindMaks >= (50+Startpunkt) & VindMaks<(75+Startpunkt)){Farge="darkblue"}
+          if (VindMaks >= (25+Startpunkt) & VindMaks<(50+Startpunkt)){Farge="darkgreen"}
+        }
+      }
+      #      TextOnStaticMap(Kart,lon=as.numeric(St$LON_DEC[n]),lat=as.numeric(St$LAT_DEC[n]),col='red',labels=VindMaks,adj=c(+1.5),cex=0.5,add=TRUE)
       if(is.finite(VindMaks)){
         if(is.na(MinGrense)|VindMaks>=MinGrense){
           TextOnStaticMap(Kart,lon=as.numeric(St$LON_DEC[n]),lat=as.numeric(St$LAT_DEC[n]),col=Farge,labels=VindMaks,cex=0.7,add=TRUE)
