b9696624a2763a52915b280bbb0554f02a8902c5
Hans Olav Hygen <hohygen@gmail.com>
2026-02-23 14:42:29 +0100

    Refactor DUT function for design temperature retrieval
    Updated the DUT function to fetch design temperature data from a new API endpoint and modified the data extraction logic for summer and winter datasets.

--- R/DUT.R
+++ R/DUT.R
@@ -1,21 +1,26 @@
 library(jsonlite)
 
-#Funksjon som henter ut DUT for en gitt kommune. Kallet er kommunenummeret med KS forra, Oslo er KS301 Velg mellom sommer og vinter vad å sette T eller F på Winter.
-
-DUT <- function(Kommune="KS301",Winter=T){
-  #fromJSON("https://frost-beta.met.no/api/v1/reports/get?type=dut&settings=%7B%22SourceID%22%3A%22KS301%22%7D")
-  URL <- paste("https://ca076a66-7ea1-4ca2-8eaf-f091899a9eba:f8413549-8f24-4ce1-8154-e5a2c85e553c@frost-beta.met.no/api/v1/reports/get?type=dut&settings=%7B%22SourceID%22%3A%22",Kommune,"%22%7D",sep="")
+DUT <- function(Kommune="301",Winter=T){
+  URL <- paste("https://ca076a66-7ea1-4ca2-8eaf-f091899a9eba:f8413549-8f24-4ce1-8154-e5a2c85e553c@frost.met.no/api/v1/designtemp?municipalityids=",Kommune,sep="")
   DUTdata <- fromJSON(URL)
-  Datasett <-  DUTdata$data$summer
+  Datasett <-  DUTdata$municipalities$data$Summer
   Tittel <- paste("Design temperature summer for ", Kommune, sep="")
   plass = "bottomright"
   if(Winter){
-    Datasett <-  DUTdata$data$winter
+    Datasett <-  DUTdata$municipalities$data$Winter
     Tittel <- paste("Design temperature winter for ", Kommune, sep="")
     plass = "topright"
   }
-  plot(Datasett[,4],Datasett[,2],pch=16,col=farver[Datasett[,1]],
-       ylab="Temperature",xlab = "Return period (years)", main=Tittel)
-  legend(plass, legend=c("1 day","2 day","3 day","4 day","5 day"), col=farver, pch=16)
+  farver <- rainbow(9)
+  YRange <- c(min(Datasett[[1]][,3]),max(Datasett[[1]][,3]))
+  plot(-1000,-1000,pch=16,ylim=YRange,xlim=c(1,5),
+       ylab="Temperature",xlab = "Duration (days)", main=Tittel)
+  legend(plass, legend=c("2","5","10","20","25","30","50","100","200"), col=farver, pch=16)
+  n=0
+  for(f in c(2,5,10,20,25,30,50,100,200)){
+    n=n+1
+    lines(Datasett[[1]][Datasett[[1]][,2]==f,1], Datasett[[1]][Datasett[[1]][,2]==f,3], pch=16, col=farver[n])
+  }
+  
   Datasett
 }
